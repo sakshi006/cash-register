06997f2d075175eb4b39596b2162531d850043d6
Sakshi Kumar <Saks5011@gmail.com>
2020-12-28 09:09:26 +0530

    fix : js linked

---
+++ cash-register/app.js
@@ -0,0 +1,12 @@
+const bill = document.querySelector(".bill");
+const cash = document.querySelector(".cash");
+const buttonCheck = document.querySelector(".check");
+const amt2000 = document.querySelector(".note-2000");
+const amt1000 = document.querySelector(".note-1000");
+const amt500 = document.querySelector(".note-500");
+const amt100 = document.querySelector(".note-100");
+const amt20 = document.querySelector(".note-20");
+const amt10 = document.querySelector(".note-10");
+const amt5 = document.querySelector(".note-5");
+const amt1 = document.querySelector(".note-1");
+
